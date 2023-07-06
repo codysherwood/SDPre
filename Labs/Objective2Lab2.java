b208153e39c1ed383c129204553355dbb6b6c7fd
Cody Comstock <mr.codysherwood@hotmail.com>
2023-07-06 11:54:27 -0500

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Cody Comstock";
+		int age = 33;
+		String state = "Arkansas";
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
