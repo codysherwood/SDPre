73cf204f431cc7c2ae266f878aa9917bddc8ff28
Cody Comstock <mr.codysherwood@hotmail.com>
2023-07-06 15:17:42 -0500

    complete objective3lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,7 @@
+public class Objective3Lab1 {
+	public static void main(String[] args) {
+		double num1 = 3, num2 = 6, num3 = 9, average = (num1 + num2 + num3) / 3;
+		System.out.println(average);
+
+	}
+}
