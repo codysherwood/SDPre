b231d4e2d1fb0431eda412d767f5eec08a12fb36
Cody Comstock <mr.codysherwood@hotmail.com>
2023-07-10 14:01:14 -0500

    objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,9 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 1;
+		for (int count = 2; count < 21; count ++) {
+			sum += count;
+		}
+		System.out.println(sum);
+	}
+}
