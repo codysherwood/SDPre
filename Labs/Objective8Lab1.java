ce51892ae6d42f260dc7c7279a81c5e93c6e4235
Cody Comstock <mr.codysherwood@hotmail.com>
2023-07-10 12:48:12 -0500

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+	public static void main(String[] args) {
+		for (int i1 = 1; i1<11; i1++) {
+			System.out.println(i1);
+		}
+		for (int i2=10; i2>0; i2--){
+			System.out.println(i2);
+		}
+	}
+}
