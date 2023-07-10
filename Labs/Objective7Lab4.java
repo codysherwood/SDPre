6a96525979eff20f5fb126c7f18344e032e5f61a
Cody Comstock <mr.codysherwood@hotmail.com>
2023-07-10 11:25:24 -0500

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,14 @@
+public class Objective7Lab4 {
+	public static void main(String[] args) {
+		int count = 2;
+		int sum = 1;
+
+		while (count < 21) {
+			sum = count + sum;
+			count ++;
+		}
+		System.out.println(sum);
+
+
+	}
+}
